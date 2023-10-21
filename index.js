73f466a015ed145f8b6e16852a959f749c93f72d
Tayla Rios <145400166+taylasagerios@users.noreply.github.com>
2023-10-20 22:36:41 -0700

    Added in first section input title

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 inquirer
 .prompt{[
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"  
+    },
     
 ]}
